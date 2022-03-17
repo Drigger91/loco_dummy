9d3b2cbbac6d36a97cee8718489654f022581664
Drigger91 <82764734+Drigger91@users.noreply.github.com>
2022-03-17 16:03:01 +0530

    last commit

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import styles from './home.module.css'
+import React, { useEffect } from 'react'
+import styles from './home.module.css' 
+import gsap from 'gsap'
 function Home() {
+  useEffect(()=>{
+    setTimeout(() => {
+      gsap.to('.image',{x:400,duration:8,rotation:"+=360"})
+    }, 1000);
+  },[])
   return (
     <div className={styles.home}>
         <h1>Home</h1>
         <h2>
             This is the basic page with just smooth scroll enabled and basic gsap animation
         </h2>
-        <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/16327/logo-man.svg'/>
+        <img className='image' src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/16327/logo-man.svg'/>
         </div>
   )
 }
